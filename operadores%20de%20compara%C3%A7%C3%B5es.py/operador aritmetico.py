fe4b4cccbab3a4e485fffc4fa24bed6a3c3c906b
Jadson1834 <138531380+Jadson1834@users.noreply.github.com>
2023-10-25 14:01:14 -0300

    Create operador aritmetico.py

---
+++ operadores%20de%20compara%C3%A7%C3%B5es.py/operador aritmetico.py
@@ -0,0 +1,12 @@
+nota_1 = 6
+nota_2 = 7
+nota_3 = 8
+nota_4 = 9
+print(nota_1+nota_4)
+print(nota_1**nota_3)
+print(nota_4*nota_2)
+print(nota_2%nota_4)
+print(nota_3-nota_4)
+print(nota_2/nota_3)
+x = (23+43)-8*((5-33)+5**2)
+print(x)
