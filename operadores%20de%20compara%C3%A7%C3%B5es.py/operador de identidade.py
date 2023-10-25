ec3e7d52921beb75d4707ce3ee3ac61fe6a9efec
Jadson1834 <138531380+Jadson1834@users.noreply.github.com>
2023-10-25 13:52:09 -0300

    Create operador de identidade.py

---
+++ operadores%20de%20compara%C3%A7%C3%B5es.py/operador de identidade.py
@@ -0,0 +1,18 @@
+humano = 'homem'
+cara = 'pessoa'
+gente = humano
+print(cara is gente)
+print(cara is not gente)
+print(humano is not gente)
+
+
+
+saque = 300.0
+limite = 300
+print(saque is limite)
+print(saque is not limite)
+
+dinheiro = 200
+money = 200
+print(dinheiro is limite)
+print(dinheiro is not limite)
