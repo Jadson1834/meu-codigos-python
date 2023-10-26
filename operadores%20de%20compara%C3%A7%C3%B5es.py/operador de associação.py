91db8809cae3dc110b82f15d4c1ef9da1041a594
Jadson1834 <138531380+Jadson1834@users.noreply.github.com>
2023-10-26 18:21:32 -0300

    Create operador de associação.py

---
+++ operadores%20de%20compara%C3%A7%C3%B5es.py/operador de associação.py
@@ -0,0 +1,14 @@
+cara = 'pessoa bem humorada'
+fruta = ['laranja','maça','uva']
+saque = [100,150]
+
+print('pessoa' in cara)
+print('humorada' not in cara)
+print('bem' in cara)
+print(cara not in 'Pessoa')
+
+print('Laranja' in fruta)
+print('uva' in fruta)
+print('maca' not in fruta)
+
+print(100 in saque)
