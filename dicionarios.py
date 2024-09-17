0d4facb7ee149c7702eccdad0cc515a16dcc7f4c
Jadson1834 <138531380+Jadson1834@users.noreply.github.com>
2024-09-17 20:58:23 -0300

    Create dicionarios.py

---
+++ dicionarios.py
@@ -0,0 +1,23 @@
+# Crie um dicionário para armazenar os nomes, telefones e emails dos seus amigos.
+dicionario = {
+"nome": "carlos","telefone": "98966-5656","email": "calosalberto@gmail.com"}
+
+# Crie um dicionário com os itens que você precisa comprar, incluindo a quantidade e o preço.
+comprar = {"item":"pasta de dente","quantidade":5,"preço":"R$7,00"}
+# Crie um dicionário onde as chaves são nomes de cores e os valores são códigos hexadecimais dessas 
+cores = {"cor1":{"Nome":"vermelho","hexadecimal":"#ff0000"},"cor2":{"Nome":"amarelo","hexadecimal1":"#ffff00"}}
+# Crie um dicionário para armazenar informações de alunos, incluindo nome, idade e curso.
+informaçoes_alunos = {"aluno1":{"nome":"alberto","idade":19,"curso":"informatica"},
+                    "aluno2":{"nome":"marcos","idade":24,"curso":"enfermagem"}}
+# Crie um dicionário para armazenar produtos de uma loja, incluindo o nome do produto, quantidade em estoque e preço.
+loja = {"produto1":{"Nome":"shampo","quantidade":7,"preço":"R$10,00"},
+        "produto2":{"Nome":"ketchup","quantidade":9,"preço":"R$8,00"}}
+# Crie um dicionário para armazenar livros de uma biblioteca, incluindo título, autor e ano de publicação.
+livros_biblioteca = {"livro1":{"Titulo":"IT a coisa","autor":" Stephen King","ano_publicalao":"2014"},
+                     "livro2":{"Titulo":"Nao me faça pensar","autor":" Steve Krug","ano_publicalao":"2000"}}
+# Crie um dicionário para armazenar filmes, incluindo título, diretor e ano de lançamento.
+filmes = {"Filme1":{"nome":"Transformers","Produtor":"Michael Bay","ano de lançamento":"2007"},
+          "Filme2":{"nome":"Divertidamente","Produtor":"Pete Docter","ano de lançamento":"2015"}}
+# Crie um dicionário para registrar vendas de uma loja, incluindo o nome do cliente, produto comprado e valor da compra.
+vendas_loja = {"cliente1":{"nome":"mateus","produto comprado":"agua","valor da compra":"R$2,00"},
+               "cliente2":{"nome":"ryan","produto comprado":"margarida","valor da compra":"R$4,00"}}
